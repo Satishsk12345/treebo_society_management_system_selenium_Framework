83e85e6b0d4ce2ab9b822a0f5e271fc90b103405
user <user@DESKTOP-9CD1PJA>
2022-09-21 12:41:03 +0530

    conflit in old

--- src/main/java/treebo/genericUtility/commonPage.java
+++ src/main/java/treebo/genericUtility/commonPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//conflit in old
 
 public class commonPage {
 	//@FindBy(xpath = "//a[.=' Activities']")
